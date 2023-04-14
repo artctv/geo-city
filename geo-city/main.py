3bf60014488ff00310dd4c54fb8bd0619e362b7e
Artem 'artctv <artctvcode@gmail.com>
2023-04-14 21:05:39 +0500

    add writing to file, info prints and thread

--- geo-city/main.py
+++ geo-city/main.py
@@ -1,15 +1,46 @@
-from multiprocessing import Queue
+import time
+from multiprocessing import Queue, Process
 from copy import deepcopy
 from loader import get_data
 from process import calculate
+from config import Config
 
 
 def main(temp: bool = False, data_count: int = 0):
+    print("--- Script started ---")
+
     data = get_data(temp, data_count)
+    print("--- Data loaded ---")
 
     queue: Queue = Queue()
     for i in data:
         queue.put(deepcopy(i))
+    print("--- Multiprocessing patterns inited ---")
 
-    calculate(queue, data)
+    processes: list[Process] = []
+    for i in range(Config.CPU_COUNT):
+        p = Process(target=calculate, args=(i+1, queue, data))
+        processes.append(p)
+
+    print("--- Processes created ---")
+
+    for p in processes:
+        p.start()
+
+    print("--- Processes started ---")
+
+    while not queue.empty():
+        print(f"--- Queue size: {queue.qsize()} ---")
+        time.sleep(Config.SLEEP_TIME)
+
+    print("--- Data calculated ---")
+    for p in processes:
+        p.join()
+
+    print("--- Processes completed ---")
+
+    for p in processes:
+        p.terminate()
+
+    print("--- Processes terminated ---")
 
--- geo-city/process.py
+++ geo-city/process.py
@@ -1,5 +1,8 @@
+from queue import Queue as TQueue
 from multiprocessing import Queue
+from threading import Event, Thread
 import array
+import pathlib
 from config import Config
 from calculation.calculation import combinations
 
@@ -8,12 +11,31 @@ def get_string_format(city_1: str, city_2: str, distance: float) -> str:
     return Config.STRING_PATTERN.format(city_1=city_1, city_2=city_2, distance=distance)
 
 
+def write_thread(queue: TQueue, e: Event, file: pathlib.Path):
+    counter: int = 0
+    with open(file, "w") as f:
+        while True:
+            _str = queue.get()
+            if _str:
+                f.write(_str)
+                counter += 1
+
+            if counter >= Config.FLUSH_COUNT:
+                f.flush()
+                counter = 0
+
+            if e.is_set():
+                break
+
+
 def calculate(
+    n: int,
     queue: Queue,
     data: list[tuple[str, float, float]]
 ):
-    counter: int = 0
     cities, coordinates = [], []
+    file = Config.TEMP_FOLDER / Config.PROCES_TMP_PATTERN.format(n)
+
     for i in data:
         cities.append(i[0])
         coordinates.append(i[1])
@@ -24,9 +46,18 @@ def calculate(
     coordinates.clear()
     result_array: array.array = array.array("i", [0 for i in range(values_size)])
 
+    t_queue, e = TQueue(), Event()
+    thread = Thread(target=write_thread, args=(t_queue, e, file))
+    thread.start()
+
     while not queue.empty():
         city, lon_1, lat_1 = queue.get()
         distances = combinations(lon_1, lat_1, values, values_size, result_array)
+
         for city_2, distance in zip(cities, distances):
-            print(city, city_2, distance)
+            _str = get_string_format(city, city_2, distance)
+            t_queue.put(_str)
+
+    e.set()
+    thread.join()
 
