1900b5188a77adee66a19371b75e58c3fc85354e
Artem 'artctv <artctvcode@gmail.com>
2023-04-14 21:04:54 +0500

    add nogil

--- geo-city/calculation/calculation.pyx
+++ geo-city/calculation/calculation.pyx
@@ -5,11 +5,10 @@ from libc.string cimport memset
 from cpython cimport array
 import array
 
-
 cdef extern from "module.c" nogil:
     int* combinations_impl(double lon_1, double lat_1, double *values, int values_size);
 
-cdef int* harvesine_array(double lon_1, double lat_1, double *elemts_values, int elemts_size):
+cdef int* harvesine_array(double lon_1, double lat_1, double *elemts_values, int elemts_size) nogil:
     return combinations_impl(lon_1, lat_1, elemts_values, elemts_size);
 
 cdef pythonize(int *ptr, int values_size, array.array result_array):
@@ -26,7 +25,8 @@ def combinations(
     result_array: array.array
 ) -> array.array:
     cdef int* result;
-    result = harvesine_array(lon_1, lat_1, values.data.as_doubles, values_size);
+    with nogil:
+        result = harvesine_array(lon_1, lat_1, values.data.as_doubles, values_size);
     result_array = pythonize(result, values_size, result_array)
     free(result)
     return result_array
