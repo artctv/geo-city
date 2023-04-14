e9a5fff4fa7911cfeca1bfd1162cda1fe9c15e51
Artem 'artctv <artctvcode@gmail.com>
2023-04-14 10:57:44 +0500

    add simple cli

---
+++ geo-city/__main__.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Load data param")
+parser.add_argument(
+    "-t", "--temp",
+    default=False,
+    action=argparse.BooleanOptionalAction,
+    help="load from temp data or not"
+)
+parser.add_argument(
+    "-dc", "--data-count",
+    nargs="?",
+    default=0,
+    help="how many data need to load, by default load all"
+)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
