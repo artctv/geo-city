724f7003c804e92e336006d80b663fee72d0b739
Artem 'artctv <artctvcode@gmail.com>
2023-04-15 15:27:03 +0500

    update process task

--- geo-city/process.py
+++ geo-city/process.py
@@ -1,31 +1,12 @@
-from queue import Queue as TQueue
+import csv
 from multiprocessing import Queue
-from threading import Event, Thread
 import array
-import pathlib
 from config import Config
 from calculation.calculation import combinations
 
 
 def get_string_format(city_1: str, city_2: str, distance: float) -> str:
     return Config.STRING_PATTERN.format(city_1=city_1, city_2=city_2, distance=distance)
-
-
-def write_thread(queue: TQueue, e: Event, file: pathlib.Path):
-    counter: int = 0
-    with open(file, "w") as f:
-        while True:
-            _str = queue.get()
-            if _str:
-                f.write(_str)
-                counter += 1
-
-            if counter >= Config.FLUSH_COUNT:
-                f.flush()
-                counter = 0
-
-            if e.is_set():
-                break
 
 
 def calculate(
@@ -46,18 +27,19 @@ def calculate(
     coordinates.clear()
     result_array: array.array = array.array("i", [0 for i in range(values_size)])
 
-    t_queue, e = TQueue(), Event()
-    thread = Thread(target=write_thread, args=(t_queue, e, file))
-    thread.start()
+    with open(file, 'w', newline='') as csvfile:
+        writer = csv.writer(
+            csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL
+        )
+        writer.writerow(["City 1", "City 2", "Distance"])
 
-    while not queue.empty():
-        city, lon_1, lat_1 = queue.get()
-        distances = combinations(lon_1, lat_1, values, values_size, result_array)
+        data: list[tuple[str, str, int]]
+        while not queue.empty():
+            city_1, lon_1, lat_1 = queue.get()
+            distances = combinations(lon_1, lat_1, values, values_size, result_array)
 
-        for city_2, distance in zip(cities, distances):
-            _str = get_string_format(city, city_2, distance)
-            t_queue.put(_str)
+            for city_2, distance in zip(cities, distances):
+                writer.writerow([city_1, city_2, distance])
 
-    e.set()
-    thread.join()
+            csvfile.flush()
 
